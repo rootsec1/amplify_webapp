8b9bb814a833dd5fb41570df57f4e0a129246dc9
abhishekwl <abhishekwl@protonmail.com>
2020-03-27 09:31:52 +0530

    Created sign in page

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import 'typeface-roboto';
+import { MuiThemeProvider } from '@material-ui/core/styles';
+import { BrowserRouter } from 'react-router-dom';
+//LOCAL
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import theme from './theme';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MuiThemeProvider theme={theme}>
+        <App />
+      </MuiThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
---
+++ src/theme/index.js
@@ -0,0 +1,12 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import deepPurple from '@material-ui/core/colors/deepPurple';
+import deepOrange from '@material-ui/core/colors/deepOrange';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: deepPurple,
+        secondary: deepOrange
+    }
+});
+
+export default theme;
